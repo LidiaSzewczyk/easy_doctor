ed97b0640bccb96f80b4a5d1bd57f57d5c081efe
Lidia Szewczyk <lidia.szewczyk@gmail.com>
2023-08-28 11:57:40 +0200

    added models to visit , customization admin

--- visit/admin.py
+++ visit/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
 
-from visit.models import Diagnosis
+from visit.models import Diagnosis, Priority, Choice, Text, Part, FormType
+
+
+class PriorityInline(admin.TabularInline):
+    model = Priority
+    show_change_link = True
+    extra = 0
+    fields = ['part', 'priority', 'text', 'diagnosis']
+
+
+class ChoicesInline(admin.TabularInline):
+    model = Choice
+    show_change_link = True
+    extra = 0
+    fields = ['text', 'doctor_choice', 'patient_choice', 'priority']
 
 
 class DiagnosisAdmin(admin.ModelAdmin):
@@ -8,6 +22,42 @@ class DiagnosisAdmin(admin.ModelAdmin):
     search_fields = ('name', 'number', 'info')
     list_display_links = ('id', 'name')
     prepopulated_fields = {'slug': ('number',)}
+    inlines = [PriorityInline]
+
+
+class PartAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('name',)
+    list_display_links = ('id', 'name')
+
+
+class PriorityAdmin(admin.ModelAdmin):
+    list_display = ('id', 'part', 'priority', 'diagnosis', 'text')
+    search_fields = ('diagnosis__number', 'diagnosis__name', 'text__doctor_text')
+    list_display_links = ('id', 'part')
+    list_filter = ('part', 'priority')
+
+
+class FormTypeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_filter = ('name',)
+    search_fields = ('name',)
+    list_display_links = ('id', 'name')
+
+
+class TextAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'doctor_text', 'patient_text', 'form_type')
+    list_filter = ('form_type',)
+    search_fields = ('title', 'doctor_text', 'patient_text',)
+    list_display_links = ('id', 'title')
+    prepopulated_fields = {'slug': ('title',)}
+    autocomplete_fields = ['form_type']
+    save_on_top = True
+    inlines = [PriorityInline, ChoicesInline]
 
 
 admin.site.register(Diagnosis, DiagnosisAdmin)
+admin.site.register(Text, TextAdmin)
+admin.site.register(Part, PartAdmin)
+admin.site.register(Priority, PriorityAdmin)
+admin.site.register(FormType, FormTypeAdmin)
--- visit/models.py
+++ visit/models.py
@@ -32,5 +32,75 @@ class Diagnosis(TimeStampedModel):
     def __str__(self):
         return self.number + ' - ' + self.name
 
+    def get_absolute_url(self):
+        return reverse('visit:diagnosis_detail', kwargs={'slug': self.slug})
 
 
+class Part(models.Model):
+    name = models.CharField(max_length=250, help_text='Nazwa', verbose_name='Nazwa')
+
+    class Meta:
+        verbose_name = 'Część'
+        verbose_name_plural = 'Część'
+
+    def __str__(self):
+        return self.name
+
+
+class Priority(models.Model):
+    part = models.ForeignKey('Part', related_name='priorities', on_delete=models.CASCADE, help_text='Część',
+                             verbose_name='Część')
+    priority = models.IntegerField(help_text='Kolejność', verbose_name='Kolejność')
+    text = models.ForeignKey('Text', related_name='priorities', on_delete=models.CASCADE, help_text='Tekst',
+                             verbose_name='Tekst')
+    diagnosis = models.ForeignKey('Diagnosis', help_text='Rozpoznanie', verbose_name='Rozpoznanie',
+                                  on_delete=models.CASCADE, related_name='priorities')
+
+    class Meta:
+        verbose_name = 'Przypisanie'
+        verbose_name_plural = 'Przypisanie'
+
+    def __str__(self):
+        return self.part.name + str(self.priority)
+
+
+class Text(TimeStampedModel):
+    title = models.CharField(max_length=200, help_text='Tytuł', verbose_name='Tytuł')
+    doctor_text = models.TextField(help_text='Dla lekarza', verbose_name='Dla lekarza', null=True, blank=True)
+    patient_text = models.TextField(help_text='Dla pacjenta', verbose_name='Dla pacjenta', null=True, blank=True, )
+    slug = models.SlugField(max_length=250, null=True, unique=True)
+    form_type = models.ForeignKey('FormType', related_name='choices', on_delete=models.CASCADE, help_text='formularz',
+                                  verbose_name='formularz', null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Tekst'
+        verbose_name_plural = 'Teksty'
+
+    def __str__(self):
+        return self.doctor_text
+
+
+class FormType(models.Model):
+    name = models.CharField(max_length=150, help_text='nazwa', verbose_name='nazwa')
+
+    class Meta:
+        verbose_name = 'Typ formularza'
+        verbose_name_plural = 'Typy formularza'
+
+    def __str__(self):
+        return self.name
+
+
+class Choice(models.Model):
+    text = models.ForeignKey('Text', related_name='choices', on_delete=models.CASCADE, help_text='Text',
+                             verbose_name='Text')
+    doctor_choice = models.TextField(max_length=500, help_text='Dla lekarza', verbose_name='Dla lekarza')
+    patient_choice = models.TextField(max_length=500, help_text='Dla pacjenta', verbose_name='Dla pacjenta')
+    priority = models.IntegerField(help_text='Kolejność', verbose_name='Kolejność')
+
+    class Meta:
+        verbose_name = 'Wybór'
+        verbose_name_plural = 'Wybory'
+
+    def __str__(self):
+        return self.doctor_choice + ' - ' + self.patient_choice
